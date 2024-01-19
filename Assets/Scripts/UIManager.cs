2be6b2fdb86427d4c90ca2b6ec015a202d75abfc
Marta Lendínez <95087997+culfri09@users.noreply.github.com>
2024-01-19 11:50:14 +0100

    Start doing advice screen

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     public void ChangeScreen(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
     }
 }
